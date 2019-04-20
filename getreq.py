0440ee44b226bbaf27cc596242d77836c8d56e16
NAMAN108282 <49680172+NAMAN108282@users.noreply.github.com>
2019-04-21 01:23:30 +0530

    Add files via upload

---
+++ getreq.py
@@ -0,0 +1,14 @@
+"""
+naman
+code-101
+Thank you for taking a look at it.
+"""
+import webbrowser
+
+chrome = webbrowser.get('chrome')
+
+new = 2
+
+url = "https://www.google.com"
+
+webbrowser.open(url,new)
